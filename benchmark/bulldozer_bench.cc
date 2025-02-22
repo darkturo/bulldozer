19b835901168c9056ecb224f120955f12bf00cdd
Arturo Escudero <darkturo@gmail.com>
2025-02-22 03:10:15 +0100

    Add an extra check to have a taste of what I should improve
    I plan to use the recalculation of the merkle very often to
    explore as much alternatives as possible in the small time possible.

--- benchmark/bulldozer_bench.cc
+++ benchmark/bulldozer_bench.cc
@@ -103,6 +103,53 @@ BENCHMARK_RELATIVE_NAMED_PARAM(merkle_tree_calculate, 1000_txs, 1000);
 BENCHMARK_RELATIVE_NAMED_PARAM(merkle_tree_calculate, 2500_txs, 1000);
 BENCHMARK_RELATIVE_NAMED_PARAM(merkle_tree_calculate, 3500_txs, 1000);
 
+void merkle_tree_recalculation(int iter, int recalc_tiems) {
+    bulldozer::MerkleTree<uint8_t> tree(2500);
+    auto gen = std::mt19937(42);
+    auto dist = std::uniform_int_distribution<uint64_t>(0, 2^64);
+    BENCHMARK_SUSPEND {
+            for (int i = 0; i < 2500; i++) {
+                uint8_t buf[32];
+                for (int j = 0; j < 4; j++) {
+                    auto d = dist(gen);
+                    std::copy(reinterpret_cast<uint8_t*>(&d),
+                              reinterpret_cast<uint8_t*>(&d) + sizeof(d),
+                              buf + j * sizeof(d));
+                }
+                std::vector<uint8_t> digest(bulldozer::DigestSize);
+                bulldozer::sha256d_digest(std::span<uint8_t>(buf),
+                                          std::span<uint8_t>(digest));
+                tree.add_txid(std::span<uint8_t>(digest));
+            }
+            std::vector<uint8_t> root(bulldozer::DigestSize);
+            tree.calculate_merkle_root(std::span<uint8_t>(root));
+    }
+
+    for (int i = 0; i < iter; i++) {
+        for (int j = 0; j < recalc_tiems; j++) {
+            std::vector<uint8_t> digest(bulldozer::DigestSize);
+            BENCHMARK_SUSPEND {
+                    uint8_t buf[32];
+                    for (int j = 0; j < 4; j++) {
+                        auto d = dist(gen);
+                        std::copy(reinterpret_cast<uint8_t*>(&d),
+                                  reinterpret_cast<uint8_t*>(&d) + sizeof(d),
+                                  buf + j * sizeof(d));
+                    }
+                    bulldozer::sha256d_digest(std::span<uint8_t>(buf),
+                                              std::span<uint8_t>(digest));
+            }
+            tree.update_coinbase(std::span<uint8_t>(digest));
+            std::vector<uint8_t> root(bulldozer::DigestSize);
+            tree.calculate_merkle_root(std::span<uint8_t>(root));
+        }
+    }
+}
+BENCHMARK_NAMED_PARAM(merkle_tree_recalculation, 1000_reps, 1000);
+BENCHMARK_RELATIVE_NAMED_PARAM(merkle_tree_recalculation, 10000_reps, 1000);
+//BENCHMARK_RELATIVE_NAMED_PARAM(merkle_tree_recalculation, 100000_reps, 100000);
+//BENCHMARK_RELATIVE_NAMED_PARAM(merkle_tree_recalculation, 1000000_reps, 1000000);
+
 
 int main(int argc, char **argv) {
     gflags::SetUsageMessage("WolfSSL SHA256 benchmark");
