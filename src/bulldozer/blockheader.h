71293d9b88c2e79bcf86fd425047a3c124070e13
Arturo Escudero <darkturo@gmail.com>
2025-02-24 21:47:08 +0100

    Add a struct to hold a BlockHeader

---
+++ src/bulldozer/blockheader.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace bulldozer {
+    namespace bitcoin {
+        constexpr int32_t Version = 0x4;
+        constexpr uint32_t DigestSize = 32;
+        struct BlockHeader {
+            int32_t version;
+            char[DigestSize] prev_block_header_digest;
+            char[DigestSize] merkle_root_digest;
+            uint32_t timestamp;
+            uint32_t bits;
+            uint32_t nonce;
+        };
+    }
+}
